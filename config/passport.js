47666c409bf1cd997e38793b77226211927d05d8
formijin <fortunate.nneji@gmail.com>
2022-01-24 08:36:25 +0100

    local login spoilt

--- config/passport.js
+++ config/passport.js
@@ -1,32 +1,51 @@
 const passport = require('passport');
-const LocalStartegy = require("passport-local").Strategy;
+const LocalStartegy = require("passport-local");
 const mongoose = require ("mongoose")
 const bcrypt = require ("bcrypt");
+const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+
 
 // Load user model
 const User = require ("../models/users");
 
-module.exports = function (passport){
+module.exports.local = function (passport){
     passport.use (new LocalStartegy ({usernameField : "email"},function (username, password, done){
         
         // Match User
         User.findOne({username:username}, function (err, user) { 
-            if (err) { return done(err); }
-            if (!user) { return done(null, false); }
-            // match password wih bcrypt
-            bcrypt.compare (password, user.password, function (err, IsMatch) { 
-                
-                if (err) { console.log(err); }
-                if (IsMatch){
-                    return done(null, user);
-                }else{
-                    return done(null, false);
-                }
+          
+          if (err) { return done(err); }
+          if (!user) { return done(null, false); }
+            
+          // match password wih bcrypt
+          bcrypt.compare (password, user.password, function (err, IsMatch) { 
+               
+            if (err) { console.log(err); }
+            if (IsMatch){
+              return done(null, user);
+            }else{
+              return done(null, false);
+            }
 
-            }); 
+          }); 
         });
        
     }));
+
+
+    passport.use(new GoogleStrategy({
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/users/auth/google/secrets"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  ));
 
     
     passport.serializeUser(function(user, done) {
@@ -39,3 +58,28 @@ module.exports = function (passport){
         });
       });
 };
+
+// module.exports.google = function (passport){
+//     passport.use(new GoogleStrategy({
+//         clientID: process.env.GOOGLE_CLIENT_ID,
+//         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//         callbackURL: "http://localhost:3000/auth/google/secrets"
+//       },
+//       function(accessToken, refreshToken, profile, cb) {
+//           console.log(profile);
+//         User.findOrCreate({ googleId: profile.id }, function (err, user) {
+//           return cb(err, user);
+//         });
+//       }
+//     ));
+
+//     passport.serializeUser(function(user, done) {
+//         done(null, user.id);
+//       });
+      
+//       passport.deserializeUser(function(id, done) {
+//         User.findById(id, function(err, user) {
+//           done(err, user);
+//         });
+//       });
+// }
